9bd9023626a5d62f1a7852a719588840a243eca1
Gustavo A. Florez B <dkmaster17@hotmail.com>
2017-02-24 14:44:18 -0300

    Problem01 Developeted

--- src/Parciales/MainP.java
+++ src/Parciales/MainP.java
@@ -10,13 +10,40 @@ package Parciales;
  * @author Vit
  */
 public class MainP {
+    
+    
+    
+    
+    void Problema1()
+{
+    int vec[]={1,5,2,4,7,30,9,8},Winners[]={0,0};
+    int Resultado, Number1 , Number2,Mayor=999;
+        
+    for ( int i = 1; i < vec.length; i++ ) {
+        
+        Number1=vec[i-1];
+        Number2=vec[i];
+        Resultado=Number1-Number2;
+        if ( Resultado<0 ) {
+        Resultado=Resultado*-1;   
+        }
+        if (Resultado<Mayor) {
+            Mayor=Resultado;
+            Winners[0]=Number1;
+            Winners[1]=Number2;
+        }  
+    }
+    System.out.println("Parejas con menor distancia:"+Winners[0]+","+Winners[1]);
+
+}
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
-        System.out.println(" donde anda");
+       MainP n= new MainP();
+       n.Problema1();
     }
-    
+    // Only Style K&R 
+
 }
